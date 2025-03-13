f557319d5c40b98a82283c9676f41f906b693e6a
sun-8 <rhdqnwjsdyd0@naver.com>
2025-03-14 02:23:25 +0900

    응답 메시지 상수화 추가

--- src/main/java/com/study/message/Message.java
+++ src/main/java/com/study/message/Message.java
@@ -3,6 +3,10 @@ package com.study.message;
 public class Message {
     public final static String SUCCESS_CODE_0000 = "0000";
     public final static String ERROR_CODE_9999 = "9999";
+    public final static String ERROR_CODE_9998 = "9998";
+    public final static String ERROR_CODE_9997 = "9997";
 
     public final static String ERROR_MESSAGE_9999 = "처리 중 오류가 발생했습니다.";
+    public final static String ERROR_MESSAGE_9998 = "유효성 검증에 실패했습니다.";
+    public final static String ERROR_MESSAGE_9997 = "파일 업로드 중 문제가 발생했습니다.";
 }
---
+++ src/main/java/com/study/message/ValidationMessage.java
@@ -0,0 +1,13 @@
+package com.study.message;
+
+public class ValidationMessage {
+    public final static String NOTBLANK = "필수 입력값입니다.";
+
+    public final static String SIZE_3_TO_4 = "3글자 이상, 5글자 미만으로 입력해주세요.";
+    public final static String SIZE_4_TO_15 = "4글자 이상, 16글자 미만으로 입력해주세요.";
+    public final static String SIZE_4_TO_99 = "4글자 이상, 100글자 미만으로 입력해주세요.";
+    public final static String SIZE_4_TO_1999 = "4글자 이상, 2000글자 미만으로 입력해주세요.";
+
+    public final static String PASSWORD_PATTERN_REGEXP = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[~@#$!%*?&])[a-zA-Z\\d~@#$!%*?&]{4,15}$";
+    public final static String PASSWORD_PATTERN_MESSAGE = "영문/숫자/특수문자를 포함하여 입력해주세요.";
+}
